3a138f92ff8b96986232bbbd1b5dfa49977fe59d
Franklin Wise <franklin@krave-n.com>
2015-02-11 18:00:07 -0800

    Initial Commit

---
+++ Bull's Eye 4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bull's Eye 4
+//
+//  Created by Adriana Gustavson on 2/11/15.
+//  Copyright (c) 2015 Adriana Gustavson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
